d8b58100cae88d8e0a2be874e3a26935fb457403
Cristinaurora <cristina.leonsarabia@gmail.com>
2020-07-27 19:28:21 +0200

    Fase 1

---
+++ src/PrimeraClase.java
@@ -0,0 +1,18 @@
+
+public class PrimeraClase {
+	
+	public static void  main(String args[]) {
+	
+		String nom = new String("Aurora");
+		String cognom1 = new String("León");
+		String cognom2 = new String("Sarabia");
+		
+		int dia = 24; 
+		int mes= 7;
+		int any= 1988;
+		
+		System.out.print(cognom1 + " " + cognom2 + ", " + nom);
+		System.out.print("\n" + dia + "/" + mes + "/" + any);
+	}
+	
+}
